51d332044e7c7bc68cebe0ca676a0dc3acbfe4cc
Niewolinsky <niewolinski@protonmail.com>
2023-03-06 10:40:28 +0100

    trying to get rid of unecessary timers

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
@@ -82,27 +81,21 @@ var (
 
 type mainModel struct {
 	state   sessionState
-	timer   timer.Model
-	timer2  timer.Model
 	players []oto.Player
 }
 
 func newModel(timeout time.Duration, players []oto.Player) mainModel {
 	m := mainModel{state: soundscape_01}
-	m.timer = timer.New(timeout)
-	m.timer2 = timer.New(timeout)
 	m.players = players
 	return m
 }
 
 func (m mainModel) Init() tea.Cmd {
 	// start the timer and spinner on program start
-	return tea.Batch(m.timer.Init(), m.timer2.Init())
+	return nil
 }
 
 func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -116,53 +109,31 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		case "n":
 			if m.state == soundscape_01 {
-				m.timer = timer.New(defaultTime)
-
 				if m.players[0].IsPlaying() {
 					m.players[0].Pause()
 				} else {
 					m.players[0].Play()
 				}
-
-				cmds = append(cmds, m.timer.Init())
 			} else {
-				m.timer2 = timer.New(defaultTime)
 
 				if m.players[1].IsPlaying() {
 					m.players[1].Pause()
 				} else {
 					m.players[1].Play()
 				}
-
-				cmds = append(cmds, m.timer2.Init())
 			}
 		}
-		switch m.state {
-		// update whichever model is focused
-		case soundscape_02:
-			m.timer2, cmd = m.timer2.Update(msg)
-			cmds = append(cmds, cmd)
-		default:
-			m.timer, cmd = m.timer.Update(msg)
-			cmds = append(cmds, cmd)
-		}
-	case timer.TickMsg:
-		var cmd1 tea.Cmd
-		var cmd2 tea.Cmd
-		m.timer2, cmd1 = m.timer2.Update(msg)
-		m.timer, cmd2 = m.timer.Update(msg)
-		cmds = append(cmds, cmd1, cmd2)
 	}
-	return m, tea.Batch(cmds...)
+	return m, nil
 }
 
 func (m mainModel) View() string {
 	var s string
 	model := m.currentFocusedModel()
 	if m.state == soundscape_01 {
-		s += lipgloss.JoinHorizontal(lipgloss.Top, focusedModelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), modelStyle.Render(m.timer2.View()))
+		s += lipgloss.JoinHorizontal(lipgloss.Top, focusedModelStyle.Render("rain"), modelStyle.Render("bells"))
 	} else if m.state == soundscape_02 {
-		s += lipgloss.JoinHorizontal(lipgloss.Top, modelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), focusedModelStyle.Render(m.timer2.View()))
+		s += lipgloss.JoinHorizontal(lipgloss.Top, modelStyle.Render("rain"), focusedModelStyle.Render("bells"))
 	}
 	s += helpStyle.Render(fmt.Sprintf("\ntab: focus next • n: new %s • q: exit\n", model))
 	return s
@@ -170,9 +141,9 @@ func (m mainModel) View() string {
 
 func (m mainModel) currentFocusedModel() string {
 	if m.state == soundscape_01 {
-		return "timer"
+		return "soundscape_01"
 	} else if m.state == soundscape_02 {
-		return "timer2"
+		return "soundscape_02"
 	}
 
 	return ""
